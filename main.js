eb413d4f3b830dd467feef18919b7c11e91356a3
anjul-Webethics <anjul.webethics@gmail.com>
2021-12-06 16:57:12 +0530

    changes in first branch

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 100,
+    height: 200,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -29,7 +29,7 @@ app.whenReady().then(() => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+  })  
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
